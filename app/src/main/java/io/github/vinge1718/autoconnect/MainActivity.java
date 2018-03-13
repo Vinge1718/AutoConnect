40049b6bee8883c58fbf1200eb9e503b2cdfe916
Vinge1718 <vinge1718@gmail.com>
2018-03-13 22:22:54 +0300

    project start

---
+++ app/src/main/java/io/github/vinge1718/autoconnect/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.vinge1718.autoconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
